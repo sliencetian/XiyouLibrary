e21654ef533cb5996b965614c6ef900dfea31dae
xyTianZhao <420315258@qq.com>
2015-10-31 21:51:28 +0800

    添加我的足迹

---
+++ src/com/tz/xiyoulibrary/activity/myfoot/MyFootActivity.java
@@ -0,0 +1,22 @@
+package com.tz.xiyoulibrary.activity.myfoot;
+
+import org.androidannotations.annotations.EActivity;
+
+import android.os.Bundle;
+
+import com.tz.xiyoulibrary.R;
+import com.tz.xiyoulibrary.activity.baseactivity.BaseActivity;
+
+/**
+ * 
+ * @author Administrator Œ“µƒ ’≤ÿ
+ */
+
+@EActivity(R.layout.activity_mycollection)
+public class MyFootActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
